a1e854cc4a1105bf12a122a6ba0fa50b42482af4
px <po.bear169@gmail.com>
2026-04-01 20:06:59 +1100

    Normal update

--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -4,15 +4,23 @@
 #include <vector>
 
 struct token {
-
+	std::string tok;
+	token_t ttype;
 };
 enum class token_t {
-	
+	ID, KEYWORD, CONST, OP, 
+	OP_PAREN, CL_PAREN,
+	OP_BLOCK, CL_BLOCK,
+	OP_BRACE, CL_BRACE,
+	SEMICOLON
 };
 class lexer {
 private:
-	static const std::vector<std::string> keyword_list;
+	inline static const std::vector<std::string> keyword_list = {
+		"auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern", "float", "for", "goto", "if", "inline", "int", "long", "register", "restrict", "return", "short", "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"
+	};
 public:
 	lexer(); 
+
 };
 #endif
